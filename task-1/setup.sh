32587c71b8c57bc78194c84fc2dd147443398f52
dagimgit config --global user.email dagimvanhaileselassie@gmail.com <hackyourfuture@HackYourFutures-MacBook-Pro.local>
2026-01-16 12:37:51 +0100

    Complete Task 1 and Task 2

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,50 @@
-# Write your code here
+#!/bin/bash
+
+echo "Creating project…"
+
+mkdir project
+cd project
+
+git init
+
+# Create files and folders
+touch README.md
+mkdir resources
+touch "resources/family picture.jpg"
+touch resources/icon.png
+touch resources/logo.png
+
+touch settings.conf
+
+mkdir -p src/database
+mkdir -p src/profile
+touch src/program.java
+
+# First commit
+git add .
+git commit -m "initial commit"
+sleep 3
+
+echo "Setup project…"
+
+echo "Welcome to my project" > README.md
+rm -rf src/profile
+rm -f "resources/family picture.jpg"
+
+git add .
+git commit -m "Setup project: update README and remove files"
+sleep 3
+
+ls resources
+echo "Setup javascript…"
+
+mv src/program.java src/program.js
+echo "console.log('JavaScript works!');" > src/program.js
+node src/program.js
+
+git add .
+git commit -m "Setup JavaScript: rename file and add JS code"
+
+ls ~
+
+echo "All done!"
